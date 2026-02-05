4963aa46c7a31fdce9a0943401ca22ea4cf29319
alikerock <alikerock@gmail.com>
2026-02-05 14:00:44 +0900

    fix: supabase import 주소

--- src/components/RecentRecipes.tsx
+++ src/components/RecentRecipes.tsx
@@ -1,5 +1,5 @@
 import { useState,useEffect } from 'react';
-import { supabase } from '../utils/supabase.ts';
+import  supabase  from '../../utils/supabase.ts';
 
 type RecipeResult = {
   id: number;
